48a5c0feeb629e902fb05deb3f034fc655cd9c93
Felix Hamburger <fh-161610@hs-weingarten.de>
2022-06-13 18:29:57 +0200

    update

--- assignment2/calibration.py
+++ assignment2/calibration.py
@@ -51,4 +51,11 @@ for i in range(len(objpoints)):
     imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
     error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
     mean_error += error
-print( "total error: {}".format(mean_error/len(objpoints)) )
+
+
+print(f"total error: {}".format(mean_error/len(objpoints)) )
+print(f"matrix":{mtx})
+print(f"newcammtx":{newcameramtx})
+print(f"rvecs: {rvecs}")
+print(f"tvecs: {tvecs}")
+print(f"dist: {dist}")
