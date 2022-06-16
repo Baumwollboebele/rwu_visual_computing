1bb390ee5adde56a113ea5d400d080030816283c
Felix Hamburger <fh-161610@hs-weingarten.de>
2022-06-16 10:58:20 +0200

    update

--- assignment2/calibration.py
+++ assignment2/calibration.py
@@ -13,33 +13,21 @@ import os
 import pickle
 
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-path_used_image = "E:\\Home\\GitHub\\rwu_visual_computing\\assignment2\\used_images\\"
-path_result = "E:\\Home\\GitHub\\rwu_visual_computing\\assignment2\\result\\"
-path_vid_images = "E:\\Home\\GitHub\\rwu_visual_computing\\assignment2\\vid_images\\"
-path_summary_results = "E:\\Home\\GitHub\\rwu_visual_computing\\assignment2\\summary_results\\"
-
-# path_used_image = "used_images/"
-# path_result = "result/"
-# path_vid_images = "vid_images/"
-# path_summary_results = "summary_results/"
+path_used_image = "used_images/"
+path_result = "result/"
+path_vid_images = "vid_images/"
+path_summary_results = "summary_results/"
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 def calibration():
-
-    # termination criteria
-    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
     objp = np.zeros((6*6,3), np.float32)
     objp[:,:2] = np.mgrid[0:6,0:6].T.reshape(-1,2)
-    # Arrays to store object points and image points from all the images.
-    objpoints = [] # 3d point in real world space
-    imgpoints = [] # 2d points in image plane.
+    objpoints = [] 
+    imgpoints = [] 
 
     # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-    images = glob.glob(path_vid_images + "*.jpg") 
-
-    # images = os.listdir(path_vid_images)
-    # images = [path_vid_images+i for i in images]
+    images = os.listdir(path_vid_images)
+    images = [path_vid_images+i for i in images]
     # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
     count = 0
@@ -48,15 +36,10 @@ def calibration():
         print("iteration1: ", i, " of ", max_it)
         img = cv.imread(fname)
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        # Find the chess board corners
         ret, corners = cv.findChessboardCorners(gray, (6,6), None)
-        # If found, add object points, image points (after refining them)
         if ret == True:
             objpoints.append(objp)
-            # corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria) # Commented out cause it was only for drawing
             imgpoints.append(corners)
-            # Draw and display the corners
-            #cv.drawChessboardCorners(img, (5,5), corners2, ret) # Commented out cause it was only for drawing
             cv.imwrite(path_used_image + "frame%d.jpg" % count, img)
             count +=1
 
@@ -92,11 +75,9 @@ def test_calibration():
         t_error, ret, mtx, roi, newcameramtx, rvecs, tvecs, dist = pickle.load(f)
         print(t_error)
 
-        # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-        images = glob.glob(path_vid_images + "*.jpg") 
-
-        # images = os.listdir(path_vid_images)
-        # images = [path_vid_images+i for i in images]
+        # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        images = os.listdir(path_vid_images)
+        images = [path_vid_images+i for i in images]
         # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 
@@ -105,7 +86,6 @@ def test_calibration():
             print(i, " of ", max_it)
             img = cv.imread(image)
             h,  w = img.shape[:2]
-            # newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
             dst = cv.undistort(img, mtx, dist, None, newcameramtx)
             x, y, w, h = roi
             dst = dst[y:y+h, x:x+w]
@@ -114,7 +94,7 @@ def test_calibration():
                 image = image.split(".")[1]
             else:
                 image = image.split("\\")[-1]
-                image = image.split(".")[0]
+                image = image.split(".")[1]
 
 
             cv.imwrite(path_result + image + "_result.jpg", dst)
